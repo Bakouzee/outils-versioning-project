c3d858e89701eff9eea750351087b99b6ecb9955
Morigan <letourneaumorigan@gmail.com>
2021-10-20 18:08:24 +0200

    Player Controller with different speeds

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    float currentSpeed;
+    [SerializeField] float normalSpeed;
+    [SerializeField] float furySpeed;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        // Initialize current speed
+        currentSpeed = normalSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Movement
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * currentSpeed;
+    }
+
+    public void SetSpeed(bool furyMode) // Called from Player scrip => change speed
+    {
+        currentSpeed = furyMode ? furySpeed : normalSpeed;
     }
 }
