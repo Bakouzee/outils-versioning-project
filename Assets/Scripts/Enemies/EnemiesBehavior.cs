417bcdd28c1c1e8276a395776ef81f16b6691714
Freddy <freddy_maupou@orange.fr>
2021-10-20 18:13:16 +0200

    Add Tags + Test script EnemyBehavior + Work on scene
    Tag Wall added
    Test Enemy with Shield rotating around
    Test Enemy dashing to the player
    Test Enemy walking to the player
    Added GameObjects on EnemyAI scene

--- Assets/Scripts/Enemies/EnemiesBehavior.cs
+++ Assets/Scripts/Enemies/EnemiesBehavior.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class EnemiesBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool withShield;
+    bool isDashing;
+
+    public float timerBeforeDash = 0f;
+    public float distance = 2.0f;
+    float timer = 0f;
+
+    Transform player;
+    public GameObject shield;
+
     void Start()
     {
-        
+        timer = Random.Range(1.5f, 3.5f);
+        player = GameObject.Find("Player").GetComponent<Transform>();
+        withShield = false;
+        isDashing = false;
+        shield.SetActive(false); 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timerBeforeDash += Time.deltaTime;
+
+       // float distance = Vector2.Distance(gameObject.transform.position, player.position);
+
+        /*if(distance >= 0 && isDashing == false)
+        {
+            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, player.position, distance) * 0.5f;
+        }*/
+
+       /* if(timerBeforeDash >= timer)
+        {
+            isDashing = true;
+            timer = Random.Range(1.5f, 3.5f);
+        }
+
+        if(isDashing == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.MoveTowards(gameObject.transform.position, player.position, distance) * 2.0f;
+            timer = 0f;
+        }*/
+
+        if(withShield == true)
+        {
+            shield.SetActive(true);
+            shield.transform.RotateAround(gameObject.transform.position, shield.transform.position, 60f);
+        }
     }
 }
