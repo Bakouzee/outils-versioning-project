44d644037c4c555a98acb3779c12b921e1f0a5c1
Freddy <freddy_maupou@orange.fr>
2021-10-21 16:12:19 +0200

    Enemy Movement and Rotation
    Worked on the enemy movement to the player and its rotation to always look the player

--- Assets/Scripts/Enemies/EnemiesBehavior.cs
+++ Assets/Scripts/Enemies/EnemiesBehavior.cs
@@ -5,51 +5,46 @@ using UnityEngine;
 public class EnemiesBehavior : MonoBehaviour
 {
     public bool withShield;
-    bool isDashing;
+    bool canDash;
 
     public float timerBeforeDash = 0f;
-    public float distance = 2.0f;
-    float timer = 0f;
+    public float speedEnemy;
 
     Transform player;
+    Rigidbody2D rb;
     public GameObject shield;
+
+    Vector2 movement;
 
     void Start()
     {
-        timer = Random.Range(1.5f, 3.5f);
         player = GameObject.Find("Player").GetComponent<Transform>();
-        withShield = false;
-        isDashing = false;
+        rb = gameObject.GetComponent<Rigidbody2D>();
         shield.SetActive(false); 
     }
 
     void Update()
     {
-        timerBeforeDash += Time.deltaTime;
-
-       // float distance = Vector2.Distance(gameObject.transform.position, player.position);
-
-        /*if(distance >= 0 && isDashing == false)
-        {
-            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, player.position, distance) * 0.5f;
-        }*/
-
-       /* if(timerBeforeDash >= timer)
-        {
-            isDashing = true;
-            timer = Random.Range(1.5f, 3.5f);
-        }
-
-        if(isDashing == true)
-        {
-            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.MoveTowards(gameObject.transform.position, player.position, distance) * 2.0f;
-            timer = 0f;
-        }*/
-
-        if(withShield == true)
+        Vector2 direction = gameObject.transform.position - player.position; // gives the direction between the enemy and the player
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; // gives the angle between the enemy and the player
+        rb.rotation = angle;                                                 // the rotation is refreshing every frame to point to the player
+        direction.Normalize();                                               
+        movement = direction;                                                
+                                                                             
+        if (withShield)
         {
             shield.SetActive(true);
             shield.transform.RotateAround(gameObject.transform.position, shield.transform.position, 60f);
         }
+    }
+
+    private void FixedUpdate()
+    {
+        MoveEnemy(movement);
+    }
+
+    void MoveEnemy(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position - (direction * speedEnemy * Time.deltaTime)); // the enemy's moving to the player at a certain speed through time
     }
 }
